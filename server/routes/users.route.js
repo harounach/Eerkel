8713f3a36d5ca5d1e94c2de6a3017cb2de494a49
Haroun Ach <harounacho21@gmail.com>
2020-12-19 22:19:45 +0100

    Add user signup route

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -5,4 +5,8 @@ router.route("/signin").get((req, res) => {
   res.json({ message: "Congratulations! user are signin" });
 });
 
+router.route("/signup").get((req, res) => {
+  res.json({ message: "Welcome to Eerkel app" });
+});
+
 module.exports = router;
