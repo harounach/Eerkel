3bd17d890d194c2fb86f87b6a4c5f77abbeb6cc0
Haroun Ach <harounacho21@gmail.com>
2020-12-19 22:30:36 +0100

    Add user signout route

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -9,4 +9,8 @@ router.route("/signup").get((req, res) => {
   res.json({ message: "Welcome to Eerkel app" });
 });
 
+router.route("/signout").get((req, res) => {
+  res.json({ message: "Signed out!" });
+});
+
 module.exports = router;
