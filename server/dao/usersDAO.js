444afdf5604e89fa5081e99c673bd475bbc41a5c
Haroun Ach <harounacho21@gmail.com>
2020-12-20 13:57:16 +0100

    Create user DAO

---
+++ server/dao/usersDAO.js
@@ -0,0 +1,44 @@
+const User = require("../models/user");
+
+module.exports = {
+  /** Creater user */
+  createUser: function (userObj, done) {
+    let newUser = new User(userObj);
+    newUser.save((err, data) => {
+      if (err) {
+        /* FAILURE */
+        console.error(err);
+        done(err);
+      } else {
+        /* SUCCESS */
+        done(null, data);
+      }
+    });
+  },
+
+  /** update user */
+  updateUser: function (filter, updateObj, done) {
+    User.findOneAndUpdate(
+      /* filter */
+      filter,
+      /* update  */
+      updateObj,
+      /* options */
+      { new: true },
+      /* callback */
+      (err, data) => {
+        if (err) {
+          /* FAILURE */
+          console.error(err);
+          done(err);
+        } else {
+          /* SUCCESS */
+          done(null, data);
+        }
+      }
+    );
+  },
+
+  /** remove user */
+  removeUser: function (filter, done) {},
+};
