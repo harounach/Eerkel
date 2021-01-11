1fb5477b4b68b019911e5f58a50809e39fedcbe8
Haroun Ach <harounacho21@gmail.com>
2021-01-11 21:01:44 +0100

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Welcome from "./Pages/Welcome/Welcome";
+import Signin from "./Pages/Signin/Signin";
+import Signup from "./Pages/Signup/Signup";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+import FriendList from "./Pages/FriendList/FriendList";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      {" "}
+      <div className="App">
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/home" component={Home} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/friendlist" component={FriendList} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Welcome from "./Pages/Welcome/Welcome";
-import Signin from "./Pages/Signin/Signin";
-import Signup from "./Pages/Signup/Signup";
-import Profile from "./Pages/Profile/Profile";
-import FriendList from "./Pages/FriendList/FriendList";
-import Home from "./Pages/Home/Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
