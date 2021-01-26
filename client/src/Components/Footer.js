494ee151ff19fa10067a9bd4d970131536cc727c
Haroun Ach <harounacho21@gmail.com>
2021-01-26 18:06:55 +0100

    Design footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,11 @@
 import "./Footer.scss";
 
 function Footer(props) {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <p className="footer-notice">© Haroun Ach 2021</p>
+    </footer>
+  );
 }
 
 export default Footer;
