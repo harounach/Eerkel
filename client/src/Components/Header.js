57863ea58ca9e6b6ce044c8ef6941f182b09d518
Haroun Ach <harounacho21@gmail.com>
2021-03-03 21:28:33 +0100

    Navigate to welcome page when clicking on Logo

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,13 +1,14 @@
 import "./Header.scss";
 import logo from "./logo.svg";
+import { Link } from "react-router-dom";
 
 function Header(props) {
   return (
     <header className="header">
       <nav className="nav">
-        <div className="logo">
+        <Link to="/" className="logo">
           <img className="logo__img" src={logo} alt="Eerkel logo" />
-        </div>
+        </Link>
       </nav>
     </header>
   );
