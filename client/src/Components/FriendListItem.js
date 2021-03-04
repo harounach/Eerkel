08e45379a964b0f81e71d1b759dea3872546dc27
Haroun Ach <harounacho21@gmail.com>
2021-03-04 16:47:11 +0100

    Add profile icon to friend list item

--- client/src/Components/FriendListItem.js
+++ client/src/Components/FriendListItem.js
@@ -1,5 +1,7 @@
 import "./FriendListItem.scss";
 
+import { PersonAdd } from "@material-ui/icons";
+
 function FriendListItem(props) {
   return (
     <li className="fl-item">
@@ -10,7 +12,9 @@ function FriendListItem(props) {
         <h3 className="fl-item__name">{props.profileName}</h3>
       </div>
 
-      <button className="fl-item__control-btn">+</button>
+      <button className="fl-item__control-btn">
+        <PersonAdd />
+      </button>
     </li>
   );
 }
